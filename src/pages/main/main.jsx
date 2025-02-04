a8f589ff4affa86ad25eb9fc211b5ca1eb8b7a30
junhui324 <junhuijang00@gmail.com>
2025-02-04 16:39:08 +0900

    Feat: 헤더 구현

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -1,5 +1,21 @@
+import Header from "../../components/header/header";
+import Footer from "../../components/footer/footer";
+import styles from "./main.module.scss";
+const main_images = import.meta.glob("../../assets/main_img/*.png", {
+  eager: true,
+});
+
 export default function Main() {
-  return <div>메인페이지
-    <div>ㅇㅇㅇ</div>
-  </div>;
+  return (
+    <div>
+      <Header />
+      <div className={styles.temp}>메인</div>
+      {/* <div>
+    {Object.values(main_images).map((img, index) => (
+      <img key={index} src={img.default} alt={`Image ${index}`} />
+    ))}
+  </div> */}
+      <Footer />
+    </div>
+  );
 }
