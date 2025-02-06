a836752e94c5b99f3ea028103a351e8bcc1c2584
junhui324 <junhuijang00@gmail.com>
2025-02-06 15:54:26 +0900

    Design: page_title 컴포넌트 추가, 설명 추가

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -1,12 +1,24 @@
 import Header from "../../components/header/header";
 import Footer from "../../components/footer/footer";
 import ContactForm from "../../components/contact_form/contactForm";
+import PageTitle from "../../components/page_title/pageTitle";
+import styles from "./contact.module.scss";
 
 export default function Contact() {
+  const title = "문의하기";
+  const image = "/assets/page_title.png";
+
   return (
     <div>
       <Header />
-      <ContactForm></ContactForm>
+      <PageTitle title={title} image={image} />
+
+      <div className={styles.description}>
+        <p>문의 사항이 있으실 경우 하기 내용을</p>
+        <p>기입해 주시면 담당자가 확인 후 빠르게 답변 드리겠습니다.</p>
+      </div>
+
+      <ContactForm />
       <Footer />
     </div>
   );
