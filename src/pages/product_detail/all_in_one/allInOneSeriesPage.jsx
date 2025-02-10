837dc6ed63cbfec58192dd5757d58eb6dbf4a8a2
devRyan9025 <alex7006@naver.com>
2025-02-10 19:58:26 +0900

    Feat: allInOneSeriesPage 구현 완료

---
+++ src/pages/product_detail/all_in_one/allInOneSeriesPage.jsx
@@ -0,0 +1,71 @@
+import { useParams } from 'react-router-dom';
+
+import Header from '../../../components/header/header';
+import Footer from '../../../components/footer/footer';
+
+import PageTitle from '../../../components/page_title/pageTitle';
+import ProductInformation from '../../../components/product/productInformation';
+import ProductSpecification from '../../../components/product/productSpecification';
+import FeatureComponent from '../../../components/feature_component/featureComponent';
+
+import styles from './allInOneSeriesPage.module.scss';
+import allInOneSeriesData from '../../../data/allInOneSeriesData';
+import allInOneFeature from '../../../data/features/allInOne';
+
+export default function AllInOneSeriesPage() {
+  const { id } = useParams();
+  const idNumber = Number(id);
+
+  const seriesData = allInOneSeriesData[idNumber];
+
+  const features = allInOneFeature;
+
+  return (
+    <div className='zuseries_page'>
+      <Header />
+      <section className='detail_page_title'>
+        <PageTitle title={seriesData.pageTitle} />
+      </section>
+
+      <section className='detail_product_info'>
+        <ProductInformation
+          imgSrc={seriesData.imgSrc}
+          about={seriesData.about}
+        />
+      </section>
+
+      <section className='detail_product_spec'>
+        <ProductSpecification
+          name={seriesData.name}
+          basicImg={seriesData.basicImg}
+          commonImg={seriesData.commonImg}
+        />
+      </section>
+
+      <section className='detail_feature'>
+        <div className={styles.detail_feature_title}>
+          <p>{seriesData.feature_title}</p>
+        </div>
+
+        <FeatureComponent features={features} />
+      </section>
+
+      <section className='detail_optimal_application_field'>
+        <div className={styles.optimal_application_field_title}>
+          <p>{seriesData.field_title}</p>
+        </div>
+        <div className='field_list'>
+          <ul className={styles.field_list}>
+            <li>접착 및 결함</li>
+            <li>전자 조립</li>
+            <li>점검</li>
+            <li>픽앤플레이스</li>
+            <li>나사 드라이빙</li>
+          </ul>
+        </div>
+      </section>
+
+      <Footer />
+    </div>
+  );
+}
