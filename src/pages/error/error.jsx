344840f2b97c3f49178734040fa04a3dac594bc2
junhui324 <junhuijang00@gmail.com>
2025-03-28 14:49:07 +0900

    Fix: 에러 페이지 화면크기에 따른 padding 크기 변경 로직 추가

--- src/pages/error/error.jsx
+++ src/pages/error/error.jsx
@@ -3,15 +3,34 @@ import { Link } from "react-router-dom";
 import styles from "./error.module.scss";
 import images from "../../constants/imagePath";
 import routes from "../../constants/routes";
+import { useTranslation } from "react-i18next";
 
 export default function Error() {
+  const { t } = useTranslation();
+  const error = t("error", { returnObjects: true });
+
   useEffect(() => {
-    document.documentElement.style.paddingTop = "0"; // <html> 요소의 padding-top을 0으로 설정
-    document.body.style.paddingTop = "0"; // <body> 요소의 padding-top도 0으로 설정
+    document.documentElement.style.paddingTop = "0";
+    document.body.style.paddingTop = "0";
 
     return () => {
-      document.documentElement.style.paddingTop = "62px"; // 기본 페이지로 돌아올 때 원래대로
-      document.body.style.paddingTop = "62px";
+      // 화면 크기에 따라 적절한 padding-top 적용
+      const updatePadding = () => {
+        if (window.innerWidth <= 768) {
+          document.body.style.paddingTop = "74px";
+        } else if (window.innerWidth <= 1023) {
+          document.body.style.paddingTop = "74px";
+        } else {
+          document.body.style.paddingTop = "124px";
+        }
+      };
+
+      updatePadding(); // 초기 실행
+      window.addEventListener("resize", updatePadding); // 화면 크기 변경 시 업데이트
+
+      return () => {
+        window.removeEventListener("resize", updatePadding); // 클린업
+      };
     };
   }, []);
 
@@ -22,16 +41,17 @@ export default function Error() {
           <div className={styles.container}>
             <div className={styles.error_content}>
               <img src={images.error} alt='Error' />
-              <h3>없는 페이지 입니다</h3>
-              <p>확인 후 다시 요청 바랍니다.</p>
+              <h3>{error.title}</h3>
+              {error.description.map((line, index) => (
+                <p key={index}>{line}</p>
+              ))}
 
               <div className={styles.btn_box}>
                 <Link to={routes.home} className={styles.default_btn}>
-                  <i className='flaticon-history'></i> 뒤로가기 <span></span>
+                  {error.back_button}
                 </Link>
                 <Link to={routes.home} className={styles.default_btn}>
-                  <i className='flaticon-earth-day'></i> 홈으로 이동{" "}
-                  <span></span>
+                  {error.home_button}
                 </Link>
               </div>
             </div>
