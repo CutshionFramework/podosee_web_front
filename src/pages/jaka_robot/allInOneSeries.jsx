c7c1dd72f096a8825a779567f65a5e611162395a
devRyan9025 <alex7006@naver.com>
2025-07-28 01:42:41 +0900

    Design: 페이지명 및 파일명 변경

--- src/pages/jaka_robot/allInOneSeries.jsx
+++ src/pages/jaka_robot/allInOneSeries.jsx
@@ -1,14 +1,14 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
-import Header from '../../components/header/header';
-import Footer from '../../components/footer/footer';
-import PageTitle from '../../components/page_title/pageTitle';
-import RobotCard from '../../components/card/robotCard';
+import Header from "../../components/header/header";
+import Footer from "../../components/footer/footer";
+import PageTitle from "../../components/page_title/pageTitle";
+import RobotCard from "../../components/card/robotCard";
 
-import data from '../../data/series_data/allInOneSeriesData';
-import styles from './allInOne.module.scss';
+import data from "../../data/series_data/allInOneSeriesData";
+import styles from "./allInOneSeries.module.scss";
 
 export default function AllInOne() {
   const { t, i18n } = useTranslation();
@@ -18,23 +18,23 @@ export default function AllInOne() {
 
   // 언어별 이미지 경로 설정
   const imagePath =
-    currentLang === 'kr'
-      ? '/assets/product_comparison/kr/jaka_all_in_one_product_comparison_kr.png'
-      : '/assets/product_comparison/en/jaka_all_in_one_product_comparison_en.png';
+    currentLang === "kr"
+      ? "/assets/product_comparison/kr/jaka_all_in_one_product_comparison_kr.png"
+      : "/assets/product_comparison/en/jaka_all_in_one_product_comparison_en.png";
 
-  const pageSubtitle = t('jaka_all_in_one.page_subtitle', {
+  const pageSubtitle = t("jaka_all_in_one.page_subtitle", {
     returnObjects: true,
   });
 
   const firstFeatureDescription = t(
-    'jaka_all_in_one.first_feature_description',
+    "jaka_all_in_one.first_feature_description",
     {
       returnObjects: true,
     }
   );
 
   const secondFeatureDescription = t(
-    'jaka_all_in_one.second_feature_description',
+    "jaka_all_in_one.second_feature_description",
     {
       returnObjects: true,
     }
@@ -49,11 +49,11 @@ export default function AllInOne() {
   return (
     <>
       <Header />
-      <section className='page_title'>
+      <section className="page_title">
         <PageTitle title={t("jaka_all_in_one.page_title")} />
       </section>
 
-      <section className='all_in_one'>
+      <section className="all_in_one">
         <div className={styles.page_subtitle}>
           {pageSubtitle.map((subTitle, index) => (
             <p key={index}>{subTitle}</p>
@@ -74,7 +74,7 @@ export default function AllInOne() {
         </div>
       </section>
 
-      <section className='major_feature'>
+      <section className="major_feature">
         <div className={styles.major_title}>
           <span>{t("jaka_all_in_one.major_title")}</span>
         </div>
@@ -92,13 +92,19 @@ export default function AllInOne() {
           </div>
 
           <div className={styles.first_feature_img}>
-            <img src='/assets/jaka_all_in_one/major_feature_1.png' alt='' />
+            <img
+              src="/assets/jaka_all_in_one/major_feature_1.png"
+              alt=""
+            />
           </div>
         </div>
 
         <div className={styles.second_feature}>
           <div className={styles.second_feature_img}>
-            <img src='/assets/jaka_all_in_one/major_feature_2.png' alt='' />
+            <img
+              src="/assets/jaka_all_in_one/major_feature_2.png"
+              alt=""
+            />
           </div>
 
           <div className={styles.second_feature_info}>
@@ -115,13 +121,13 @@ export default function AllInOne() {
         </div>
       </section>
 
-      <section className='product_comparison'>
+      <section className="product_comparison">
         <div className={styles.comparison_title}>
           <span>{t("jaka_all_in_one.comparison_title")}</span>
         </div>
 
         <div className={styles.comparison_img}>
-          <img src={imagePath} alt='제품 비교 이미지' />
+          <img src={imagePath} alt="제품 비교 이미지" />
         </div>
       </section>
       <Footer />
---
+++ src/pages/jaka_robot/miniSeries.jsx
@@ -0,0 +1,90 @@
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+
+import Header from "../../components/header/header";
+import Footer from "../../components/footer/footer";
+import PageTitle from "../../components/page_title/pageTitle";
+
+import FeatureComponent from "../../components/feature_component/featureComponent";
+
+import jakaProducts from "../../data/features/jakaProducts";
+import styles from "./miniSeries.module.scss";
+
+export default function MiniSeries() {
+  const { t, i18n } = useTranslation();
+
+  // 현재 언어 가져오기 (예: "kr", "en")
+  const currentLang = i18n.language;
+
+  // 언어별 이미지 경로 설정
+  const imagePath =
+    currentLang === "kr"
+      ? "/assets/product_comparison/kr/jaka_mini_product_comparison_kr.png"
+      : "/assets/product_comparison/en/jaka_mini_product_comparison_en.png";
+
+  const aboutList = t("jaka_mini.about", {
+    returnObjects: true,
+  });
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <>
+      <Header />
+
+      <section className="page_title">
+        <PageTitle title={t("jaka_mini.page_title")} />
+      </section>
+
+      <section className="mini_series">
+        <div className={styles.page_subtitle}>
+          <span>{t("jaka_mini.page_subtitle")}</span>
+        </div>
+
+        <section className="mini_series_information">
+          <div className={styles.product_container}>
+            <div className={styles.product_info}>
+              <section className={styles.product_img_section}>
+                <div className={styles.product_img}>
+                  <img
+                    src="/assets/jaka_mini/mini.webp"
+                    alt="제품 이미지"
+                  />
+                </div>
+              </section>
+
+              <section className={styles.product_about_section}>
+                <div className={styles.product_about}>
+                  {aboutList.map((about, index) => (
+                    <p key={index}>{about}</p>
+                  ))}
+                </div>
+              </section>
+            </div>
+          </div>
+        </section>
+      </section>
+
+      <section className="product_comparison">
+        <div className={styles.comparison_title}>
+          <span>{t("jaka_mini.comparison_title")}</span>
+        </div>
+
+        <div className={styles.comparison_img}>
+          <img src={imagePath} alt="제품 비교 이미지" />
+        </div>
+      </section>
+
+      <section className="feature_component">
+        <div className={styles.feature_title}>
+          <span>{t("jaka_mini.feature_title")}</span>
+        </div>
+        <FeatureComponent features={jakaProducts} />
+      </section>
+
+      <Footer />
+    </>
+  );
+}
